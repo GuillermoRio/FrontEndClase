2afe690e1412117fac11029103096278438b1036
Guillermo Rio <guilleriofrio12@gmail.com>
2026-02-02 16:41:30 +0100

    Clase completa

--- Semana1/proyecto1/src/App.tsx
+++ Semana1/proyecto1/src/App.tsx
@@ -1,6 +1,14 @@
 
-
+import './App.css'
+import Saludo from './components/hello'
 
 const MiFuncion = () => {
-  
+  return (
+    <div className="mainContainer">
+      <h1>Pedazo de título increíble</h1>
+      <Saludo name='Paqui'/>
+    </div>
+  )
 }
+
+export default MiFuncion
